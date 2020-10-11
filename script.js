5378305d901567006addf09adcaa289d21a90360
zainiba <47758673+zainiba@users.noreply.github.com>
2020-10-11 16:58:18 -0700

    added timer to js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function showBuyNow() {
 }
 
 //this calls the function above, 3000 milliseconds is 3 seconds, adjust here to make it a longer interval
-setTimeout("showBuyNow()", 100);
+setTimeout("showBuyNow()", 8700);
 
 var str = "<h1 class=front1>Welcome to Society.</h1> <br><h3 class=front2>We are so glad you could make it.</h3><br><br><p class=front3>Please take your required welcome balloon</p>",
     i = 0,
